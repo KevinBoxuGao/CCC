fcd852a2962630922d8fcbdb0604efe21aaa74aa
Kevin Gao <kevingao2003@gmail.com>
2018-12-23 12:12:06 -0500

    Problem B done and deleting useless files

--- 1996/Problem B.py
+++ 1996/Problem B.py
@@ -1,6 +1,21 @@
+x = input()
+y = input()
 
-num = input()
+def divisible_by_11(a):
+    b = int(two_digits(a))
+    if b % 11 == 0:
+        print("The number", a, "is divisible by 11.")
+    else:
+        print("The number", a, "is not divisible by 11.")
 
-while True:
-    x = num[len(num)] 
-    num = num[]
+def two_digits(a):
+    if len(a) <= 1:
+        return a
+    else:
+        print(a)
+        return two_digits(str(int(a[:len(a)-1]) - int(a[len(a)-1]))) 
+
+for i in y.split():
+    divisible_by_11(i)
+
+
